5fc2d96652361729e6db5a84848d2ed7fe58976e
Oscar E. Ramirez Blanco <o.ramirezb@uniandes.edu.co>
2024-10-19 00:08:33 -0500

    FE13-HU09-HU10- mejora en permisos

--- web-app/src/hooks/permissions/usePermissions.ts
+++ web-app/src/hooks/permissions/usePermissions.ts
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import httpClient from "../../services/HttpClient";
-import { CanceledError } from "axios";
+import { AxiosError, CanceledError } from "axios";
 import { Permission } from "../../interfaces/Permissions";
 
 const usePermissions = () => {
@@ -24,11 +24,42 @@ const usePermissions = () => {
     return () => controller.abort();
   };
 
-  useEffect(() => {
-    reloadPermissions(); // Carga inicial de permisos
-  }, []);
+  const [loading, setLoading] = useState(false);
 
-   return { permissions, error, reloadPermissions }; // Retornamos reloadPermissions
+  const createPermission = async (newPermission: Permission) => {
+    setLoading(true); // Inicia el estado de carga
+
+    try {
+      const res = await httpClient.post<Permission>("/system/permissions", newPermission);
+      return res.data; // Retorna el nuevo permiso creado
+    } catch (err) {
+      if (err instanceof CanceledError) return;
+
+      // Verificar si el error es de Axios
+      const axiosError = err as AxiosError;
+      setError(axiosError.message);
+    } finally {
+      setLoading(false); // Finaliza el estado de carga
+    }
+  };
+
+  const updatePermission = async (aPermission: Permission) => {
+    setLoading(true);
+
+    try {
+      const res = await httpClient.put<Permission>(`/system/permissions/${aPermission.id}`, aPermission);
+      return res.data;
+    } catch (err) {
+      if (err instanceof CanceledError) return;
+
+      const axiosError = err as AxiosError;
+      setError(axiosError.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { permissions, loading, error, reloadPermissions, updatePermission, createPermission }; // Retornamos reloadPermissions
 };
 
 export default usePermissions;
