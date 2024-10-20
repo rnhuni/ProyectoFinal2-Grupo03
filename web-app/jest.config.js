48560b3b2cee17b8e8746216b978b768551af7dc
William Ballesteros <wballesteros@arrivelogistics.com>
2024-10-20 01:11:27 -0500

    feature/FE09/HU17/ fix build and remove local test configuration

--- web-app/jest.config.js
+++ web-app/jest.config.js
@@ -7,11 +7,4 @@ export default {
     "\\.(css|scss)$": "identity-obj-proxy",
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  collectCoverage: true, // Habilita la recolección de cobertura
-  coverageReporters: ["text", "lcov"], // Agrega 'lcov' para generar el reporte en HTML
-  coverageDirectory: "coverage", // Directorio donde se guardará la cobertura
-  collectCoverageFrom: [
-    "src/**/*.{js,jsx,ts,tsx}", // Define los archivos de los cuales se recopila cobertura
-    "!src/**/*.d.ts", // Excluye archivos de declaración
-  ],
 };
