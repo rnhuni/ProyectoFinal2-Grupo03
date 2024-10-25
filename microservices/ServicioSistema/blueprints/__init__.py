57d76298b9087f44ebcf6147f440be0c498861eb
Nicolás Hüg <r.hug@uniandes.edu.co>
2024-10-24 22:45:32 -0300

    Arreglo del CH

--- microservices/ServicioSistema/blueprints/__init__.py
+++ microservices/ServicioSistema/blueprints/__init__.py
@@ -1,11 +1,15 @@
 from importlib import import_module
 import os
+from .healthcheck.routes import healthcheck_bp
 
 base_url = os.getenv("BASE_URL", "")
 
 def register_blueprints(app):
+    app.register_blueprint(healthcheck_bp, url_prefix='/')
     blueprints_dir = os.path.dirname(__file__)
     for module_name in os.listdir(blueprints_dir):
+        if module_name == "healthcheck":
+            continue
         module_path = os.path.join(blueprints_dir, module_name)
         if os.path.isdir(module_path) and not module_name.startswith('__'):
             routes_file = os.path.join(module_path, 'routes.py')
