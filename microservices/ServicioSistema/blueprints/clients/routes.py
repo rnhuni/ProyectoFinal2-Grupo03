aa4030ed980cae2b03eb5d3332b98a9012c57d60
rnhdev <r.hug@uniandes.edu.co>
2024-11-07 22:58:50 -0300

    Creación de base de endpoint para la asignación de plan a client

--- microservices/ServicioSistema/blueprints/clients/routes.py
+++ microservices/ServicioSistema/blueprints/clients/routes.py
@@ -17,3 +17,10 @@ def get_all_clients():
         } for perm in clients]), 200
     except Exception as e:
         return jsonify({'error': f'Error retrieving clients. Details: {str(e)}'}), 500
+    
+@clients_bp.route('/clients/<client_id>', methods=['PUT'])
+def update_client(client_id):
+    try:
+        return "OK", 200
+    except Exception as e:
+        return jsonify({'error': f'Error to update client. Details: {str(e)}'}), 500
